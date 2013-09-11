a5252d0b5b09b5915bbc0b94bca7bfb8d547098c
Morten V. Pedersen <morten@mortenvp.dk>
2013-09-11 17:46:52 +0200

    Add missing unit test

---
+++ test/src/test_backward_coefficient_iterator.cpp
@@ -0,0 +1,121 @@
+// Copyright Steinwurf ApS 2011-2013.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+/// @file test_backward_coefficient_iterator.cpp Unit test
+///       for the backward_coefficient_iterator
+
+#include <cstdint>
+#include <gtest/gtest.h>
+
+#include <kodo/backward_coefficient_iterator.hpp>
+#include <fifi/fifi_utils.hpp>
+
+namespace kodo
+{
+
+    template<class FieldType>
+    class dummy_layer
+    {
+    public:
+
+        // The field type used in our test
+        typedef FieldType field_type;
+
+    public:
+
+        uint32_t symbols() const
+        {
+            return m_symbols;
+        }
+
+    public:
+
+        uint32_t m_symbols;
+
+    };
+
+
+    template<class FieldType>
+    class dummy_stack :
+        public backward_coefficient_iterator<
+               dummy_layer<FieldType> >
+    { };
+
+
+}
+
+/// Run the tests typical coefficients stack
+TEST(TestBackwardCoefficientIterator, binary)
+{
+    typedef fifi::binary field_type;
+    typedef kodo::dummy_stack<field_type> stack_type;
+
+    stack_type stack;
+    stack.m_symbols = 4U;
+
+    std::vector<uint8_t> buffer(1);
+
+    fifi::set_value<field_type>(&buffer[0], 0, 1U);
+    fifi::set_value<field_type>(&buffer[0], 1, 0U);
+    fifi::set_value<field_type>(&buffer[0], 2, 1U);
+    fifi::set_value<field_type>(&buffer[0], 3, 0U);
+
+    auto from = stack_type::iterator_type::min_index(0, stack.symbols()-1);
+    auto to = stack_type::iterator_type::max_index(0, stack.symbols()-1);
+
+    EXPECT_EQ(from, stack.symbols() - 1);
+    EXPECT_EQ(to, 0);
+
+    std::cout << "From: " << from << std::endl;
+    std::cout << "To: " << to << std::endl;
+
+    auto it = stack.coefficient_iterator(&buffer[0]);
+
+    EXPECT_FALSE(it.at_end());
+    EXPECT_EQ(it.index(), 3U);
+    EXPECT_EQ(it.value(), 0U);
+
+    it.advance();
+
+    EXPECT_FALSE(it.at_end());
+    EXPECT_EQ(it.index(), 2U);
+    EXPECT_EQ(it.value(), 1U);
+
+    it.advance();
+
+    EXPECT_FALSE(it.at_end());
+    EXPECT_EQ(it.index(), 1U);
+    EXPECT_EQ(it.value(), 0U);
+
+    it.advance();
+
+    EXPECT_FALSE(it.at_end());
+    EXPECT_EQ(it.index(), 0U);
+    EXPECT_EQ(it.value(), 1U);
+
+    it.advance();
+
+    EXPECT_TRUE(it.at_end());
+
+
+    // it = stack.coefficient_iterator(&buffer[0], 1);
+
+    // EXPECT_FALSE(it.at_end());
+    // EXPECT_EQ(it.index(), 1U);
+    // EXPECT_EQ(it.value(), 0U);
+
+    // it.advance();
+
+    // EXPECT_FALSE(it.at_end());
+    // EXPECT_EQ(it.index(), 0U);
+    // EXPECT_EQ(it.value(), 1U);
+
+    // it.advance();
+
+    // EXPECT_TRUE(it.at_end());
+}
+
+
+
