4fe5236bf854cd346cbbd1ac6b4c30bd1be4717b
Martin Hundebøll <m.hundeboll@gmail.com>
2013-10-18 15:25:44 +0200

    kodo: add layer to allow decoded-aware encoding
    This patch adds a generator layer that enables the encoder to exploit
    status information from the decoder to only include innovative symbols
    in encoded packets.
    
    The layer uses the encoder_status_vector layer to check if status
    information from the decoder has been added, and if so, calls the new
    generate_status_vector_aware() function in the uniform_generator layer.
    
    Signed-off-by: Martin Hundebøll <m.hundeboll@gmail.com>
    Signed-off-to: Steinwurf ApS
    
    NB: When only one new packet is added to an encoder that uses binary
    field size, there is a 50% risk that the next non-systematic packet
    will be the null-vector, so use with care.

---
+++ src/kodo/decoded_aware_generator.hpp
@@ -0,0 +1,45 @@
+// Copyright Steinwurf ApS 2011-2013.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+#pragma once
+
+#include <cstdint>
+
+namespace kodo
+{
+
+    /// @ingroup coefficient_generator_layers
+    /// @brief The decoded aware generator will ensure that
+    ///        layer::generate_decoded_aware(uint8_t*) is called whenever the
+    ///        encoder has information about the status of the decoder. This
+    ///        will avoid the inclusion of symbols that the decoder does not
+    ///        need.
+    template<class SuperCoder>
+    class decoded_aware_generator : public SuperCoder
+    {
+    public:
+
+        /// @copydoc layer::generate(uint8_t*)
+        void generate(uint8_t *coefficients)
+        {
+            assert(coefficients != 0);
+
+            if(SuperCoder::symbols_decoded() > 0)
+            {
+                SuperCoder::generate_decoded_aware(coefficients);
+            }
+            else if(SuperCoder::rank() < SuperCoder::symbols())
+            {
+                SuperCoder::generate_partial(coefficients);
+            }
+            else
+            {
+                SuperCoder::generate(coefficients);
+            }
+        }
+    };
+}
+
+
