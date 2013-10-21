fab437a12dc619b72b3d05160b066fef6b96de77
Martin Hundebøll <m.hundeboll@gmail.com>
2013-10-21 10:03:43 +0200

    kodo: make systematic encoder storage aware
    If a user chooses to add symbols to an on-the-fly encoder in a
    non-chronological order, the systematic encoder will fail, as it uses an
    incremental counter to select the next symbol to encode (systematically).
    
    This patch adds a storage tracker layer that maintains a bitmap of
    available symbols, and a systematic encoder layer that uses the former
    to choose the next symbol to encode.
    
    Signed-off-by: Martin Hundebøll <m.hundeboll@gmail.com>
    Signed-off-to: SteinWurf ApS <contact@steinwurf.com>

---
+++ src/kodo/storage_aware_systematic_encoder.hpp
@@ -0,0 +1,80 @@
+// Copyright Steinwurf ApS 2011-2012.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+#pragma once
+
+#include <cstdint>
+#include <boost/dynamic_bitset.hpp>
+
+namespace kodo
+{
+
+    template<class SuperCoder>
+    class storage_aware_systematic_encoder : public SuperCoder
+    {
+        typedef typename SuperCoder::status_vector status_vector;
+
+    public:
+
+        /// @copdydoc layer::construct(Factory&)
+        template<class Factory>
+        void construct(Factory& the_factory)
+        {
+            SuperCoder::construct(the_factory);
+
+            m_systematic_symbols.resize(the_factory.max_symbols(), false);
+        }
+
+        /// @copydoc layer::initialize(Factory&)
+        template<class Factory>
+        void initialize(Factory& the_factory)
+        {
+            SuperCoder::initialize(the_factory);
+
+            m_systematic_symbols.reset();
+        }
+
+        /// @copydoc layer::encode(uint8_t*, uint8_t*)
+        uint32_t encode(uint8_t *symbol_data, uint8_t *symbol_header)
+        {
+            assert(symbol_data != 0);
+            assert(symbol_header != 0);
+
+            size_t next_symbol;
+            size_t systematic_count = m_systematic_symbols.count();
+            size_t symbols_initialized = SuperCoder::rank();
+            bool in_systematic_phase = systematic_count < symbols_initialized;
+
+            if(!SuperCoder::is_systematic_on() || !in_systematic_phase)
+            {
+                return SuperCoder::encode_non_systematic(symbol_data,
+                                                         symbol_header);
+            }
+
+            next_symbol = next_available_symbol();
+            m_systematic_symbols[next_symbol] = true;
+
+            return SuperCoder::encode_systematic(symbol_data, symbol_header,
+                                                 next_symbol);
+        }
+
+    private:
+
+        uint32_t next_available_symbol()
+        {
+            status_vector available;
+
+            // Mask out the out symbols that have already been sent
+            // systematically from the set of symbols in the storage layer.
+            available = ~m_systematic_symbols & SuperCoder::storage_status();
+
+            return available.find_first();
+        }
+
+        status_vector m_systematic_symbols;
+
+    };
+
+}
---
+++ src/kodo/storage_status_vector.hpp
@@ -0,0 +1,92 @@
+// Copyright Steinwurf ApS 2011-2012.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+#pragma once
+
+#include <cstdint>
+#include <boost/dynamic_bitset.hpp>
+
+namespace kodo
+{
+
+    /// @ingroup symbol_storage_layers
+    /// @brief Implements function to retrieve symbols currently available
+    ///        in the symbol storage.
+    ///
+    /// This kind of functionality is needed when an encoder should be
+    /// able to support encoding before all symbols are ready. The
+    /// coefficient generator layers uses this information to create
+    /// partial coefficient vectors if needed.
+    template<class SuperCoder>
+    class storage_status_vector : public SuperCoder
+    {
+
+    protected:
+
+        typedef uint32_t status_block;
+        typedef boost::dynamic_bitset<status_block> status_vector;
+
+    public:
+
+        /// @copdydoc layer::construct(Factory&)
+        template<class Factory>
+        void construct(Factory& the_factory)
+        {
+            SuperCoder::construct(the_factory);
+
+            m_status.resize(the_factory.max_symbols(), false);
+        }
+
+        /// @copydoc layer::initialize(Factory&)
+        template<class Factory>
+        void initialize(Factory& the_factory)
+        {
+            SuperCoder::initialize(the_factory);
+
+            m_status.reset();
+        }
+
+        /// @copydoc layer::set_symbols(const sak::const_storage&)
+        void set_symbols(const sak::const_storage &symbol_storage)
+        {
+            m_status.set();
+            SuperCoder::set_symbols(symbol_storage);
+        }
+
+        /// @copydoc layer::set_symbol(uint32_t, const sak::const_storage&)
+        void set_symbol(uint32_t index, const sak::const_storage &symbol)
+        {
+            assert(index < SuperCoder::symbols());
+
+            m_status[index] = true;
+            SuperCoder::set_symbol(index, symbol);
+        }
+
+        /// @copydoc layer::copy_into_symbols(const sak::const_storage&)
+        void copy_into_symbols(const sak::const_storage &src)
+        {
+            m_status.set();
+            SuperCoder::copy_into_symbols(src);
+        }
+
+        /// @copydoc layer::swap_symbols(std::vector<uint8_t> &)
+        void swap_symbols(std::vector<uint8_t> &symbols)
+        {
+            m_status.set();
+            SuperCoder::swap_symbols(symbols);
+        }
+
+        const status_vector& storage_status() const
+        {
+            return m_status;
+        }
+
+    private:
+
+        status_vector m_status;
+
+    };
+
+}
