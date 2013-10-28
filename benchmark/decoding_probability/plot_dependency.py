4795f4f34346cd8bf84508d0279c7d7301f32920
Janus Heide <janusheide@gmail.com>
2013-10-28 16:29:37 +0100

    added some initial plot of rank dependency

--- benchmark/decoding_probability/plot_dependency.py
+++ benchmark/decoding_probability/plot_dependency.py
@@ -37,13 +37,14 @@ query = {
 }
 
 db = ps.connect_database()
-mc = db.kodo_overhead.find(query)
+mc = db.kodo_decoding_probability.find(query)
 df = pd.DataFrame.from_records( list(mc) )
 
-df['mean'] = (df['used'].apply(sp.mean) - df['coded'].apply(sp.mean) )/ df['coded'].apply(sp.mean)
+df['dependency'] = df['rank'].apply(sp.mean, axis=0)-1
+df['x_symbols'] = df['symbols'].apply(sp.arange)
 
-sparse = df[df['testcase'] == "SparseFullRLNC"].groupby(by= ['buildername', 'symbol_size'])
-dense = df[df['testcase'] != "SparseFullRLNC"].groupby(by= ['buildername', 'symbol_size'])
+sparse = df[df['testcase'] == "SparseFullRLNC"].groupby(by= ['buildername', 'symbol_size','symbols'])
+dense = df[df['testcase'] != "SparseFullRLNC"].groupby(by= ['buildername', 'symbol_size','symbols'])
 
 from matplotlib import pyplot as pl
 from matplotlib.backends.backend_pdf import PdfPages as pp
@@ -53,25 +54,23 @@ ps.mkdir_p(PATH + "sparse")
 ps.mkdir_p(PATH + "dense")
 pdf = pp(PATH + "all.pdf")
 
-for (buildername,symbols), group in sparse:
-    ps.set_sparse_plot()
-    p = group.pivot_table('mean',  rows='symbols', cols=['benchmark','density']).plot()
-    ps.set_plot_details(p, buildername)
-    p.set_yscale('log')
-    pl.ylabel("Overhead [\%]")
-    pl.xticks(list(sp.unique(group['symbols'])))
+for (buildername, symbol_size, symbols), group in sparse:
+    # ps.set_sparse_plot()
+    p = group.pivot_table('dependency',  rows='x_symbols', cols=['benchmark','density']).plot()
+    # ps.set_plot_details(p, buildername)
+    # pl.ylabel("Extra symbols" + " [" + list(group['unit'])[0] + "]")
+    # pl.xticks(list(sp.unique(group['symbols'])))
     pl.savefig(PATH + "sparse/" + buildername + '.eps')
     pdf.savefig(transparent=True)
 
-for (buildername,symbols), group in dense:
-    ps.set_dense_plot()
-    p = group.pivot_table('mean',  rows='symbols', cols=['benchmark','testcase']).plot()
-    ps.set_plot_details(p, buildername)
-    p.set_yscale('log')
-    pl.ylabel("Overhead [\%]")
-    pl.xticks(list(sp.unique(group['symbols'])))
-    pl.savefig(PATH + "sparse/" + buildername + '.eps')
-    pdf.savefig(transparent=True)
+# for (buildername,symbols), group in dense:
+#     ps.set_dense_plot()
+#     p = group.pivot_table('mean',  rows='symbols', cols=['benchmark','testcase']).plot()
+#     ps.set_plot_details(p, buildername)
+#     pl.ylabel("Extra symbols" + " [" + list(group['unit'])[0] + "]")
+#     pl.xticks(list(sp.unique(group['symbols'])))
+#     pl.savefig(PATH + "dense/"+ buildername + '.eps')
+#     pdf.savefig(transparent=True)
 
 pdf.close()
 
