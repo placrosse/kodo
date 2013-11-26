882275940a0511fd08a38b275969b30b4b51027f
Morten V. Pedersen <morten@mortenvp.dk>
2013-11-26 17:17:18 +0100

    Adding missing unit tests

---
+++ test/src/test_remote_pivot_aware_generator.cpp
@@ -0,0 +1,98 @@
+// Copyright Steinwurf ApS 2011-2013.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+/// @file test_remote_pivot_aware_generator.cpp Unit tests for the
+///       kodo::remote_pivot_aware_generator layer
+
+#include <cstdint>
+#include <gtest/gtest.h>
+
+#include <kodo/remote_pivot_aware_generator.hpp>
+
+namespace kodo
+{
+
+    // Put dummy layers and tests classes in an anonymous namespace
+    // to avoid violations of ODF (one-definition-rule) in other
+    // translation units
+    namespace
+    {
+
+        // Small helper struct which provides the API needed by the
+        // remote_pivot_aware_generator layer.
+        struct dummy_layer
+        {
+            typedef uint32_t rank_type;
+
+            rank_type remote_rank() const
+            {
+                rank_type rank = 0;
+                for(uint32_t i = 0; i < m_remote_is_symbol_pivot.size(); ++i)
+                {
+                    rank += m_remote_is_symbol_pivot[i];
+                }
+                return rank;
+            }
+
+            bool can_generate() const
+            {
+                return m_can_generate;
+            }
+
+            bool remote_is_symbol_pivot(uint32_t index) const
+            {
+                return m_remote_is_symbol_pivot[index];
+            }
+
+            bool can_generate(uint32_t index) const
+            {
+                return m_is_symbol_pivot[index];
+            }
+
+            bool m_can_generate;
+
+            std::vector<bool> m_remote_is_symbol_pivot;
+            std::vector<bool> m_is_symbol_pivot;
+
+        };
+
+        // Instantiate a stack containing the remote_pivot_aware_generator
+        class dummy_stack
+            : public remote_pivot_aware_generator<
+                     dummy_layer>
+          { };
+    }
+}
+
+/// Run the tests typical coefficients stack
+TEST(TestRemotePivotAwareGenerator, api)
+{
+
+    kodo::dummy_stack stack;
+
+    uint32_t symbols = 10;
+
+    stack.m_remote_is_symbol_pivot.resize(symbols, false);
+    stack.m_is_symbol_pivot.resize(symbols, false);
+
+    stack.m_can_generate = false;
+    EXPECT_EQ(stack.can_generate(), false);
+
+    stack.m_can_generate = true;
+    EXPECT_EQ(stack.can_generate(), true);
+
+    stack.m_is_symbol_pivot[3] = true;
+
+    EXPECT_EQ(stack.can_generate(3), true);
+    stack.m_remote_is_symbol_pivot[3] = true;
+
+    EXPECT_EQ(stack.can_generate(3), false);
+
+    EXPECT_EQ(stack.remote_rank(), 1U);
+    EXPECT_EQ(stack.can_generate(), false);
+    stack.m_can_generate = false;
+
+    EXPECT_EQ(stack.can_generate(), false);
+}
--- test/src/test_sliding_window_generator.cpp
+++ test/src/test_sliding_window_generator.cpp
@@ -3,13 +3,13 @@
 // See accompanying file LICENSE.rst or
 // http://www.steinwurf.com/licensing
 
-/// @file test_on_the_fly_generator.cpp Unit tests for the
-///       kodo::on_the_fly_generator
+/// @file test_sliding_window_generator.cpp Unit tests for the
+///       kodo::sliding_window_generator
 
 #include <cstdint>
 #include <gtest/gtest.h>
 
-#include <kodo/rlnc/on_the_fly_generator.hpp>
+#include <kodo/rlnc/sliding_window_generator.hpp>
 
 namespace kodo
 {
@@ -21,7 +21,7 @@ namespace kodo
     {
 
         // Small helper struct which provides the API needed by the
-        // on_the_fly_generator layer.
+        // sliding_window_generator layer.
         struct dummy_layer
         {
 
@@ -43,6 +43,17 @@ namespace kodo
                 return rank;
             }
 
+            uint32_t remote_rank() const
+            {
+                uint32_t rank = 0;
+                for(uint32_t i = 0; i < m_symbols; ++i)
+                {
+                    rank += m_remote_is_symbol_pivot[i];
+                }
+
+                return rank;
+            }
+
             uint32_t symbols() const
             {
                 return m_symbols;
@@ -53,15 +64,21 @@ namespace kodo
                 return m_is_symbol_pivot[index];
             }
 
+            bool remote_is_symbol_pivot(uint32_t index) const
+            {
+                return m_remote_is_symbol_pivot[index];
+            }
+
             uint32_t m_symbols;
             uint32_t m_coefficient_vector_size;
             std::vector<bool> m_is_symbol_pivot;
+            std::vector<bool> m_remote_is_symbol_pivot;
 
         };
 
-        // Instantiate a stack containing the on_the_fly_generator
+        // Instantiate a stack containing the sliding_window_generator
         class dummy_stack
-            : public on_the_fly_generator<
+            : public sliding_window_generator<
                      dummy_layer>
         { };
 
@@ -70,13 +87,14 @@ namespace kodo
 
 
 /// Run the tests typical coefficients stack
-TEST(TestOnTheFlyGenerator, api)
+TEST(TestSlidingWindowGenerator, api)
 {
     kodo::dummy_stack stack;
 
     stack.m_symbols = 5;
     stack.m_coefficient_vector_size = 10;
     stack.m_is_symbol_pivot.resize(stack.m_symbols, false);
+    stack.m_remote_is_symbol_pivot.resize(stack.m_symbols, false);
 
     std::vector<uint8_t> buffer(stack.m_coefficient_vector_size);
 
@@ -126,5 +144,56 @@ TEST(TestOnTheFlyGenerator, api)
     EXPECT_TRUE(stack.can_generate(2));
     EXPECT_TRUE(stack.can_generate(3));
     EXPECT_TRUE(stack.can_generate(4));
+
+    // Now start specifying some remote pivot positions
+
+    stack.m_remote_is_symbol_pivot[0] = true;
+    EXPECT_FALSE(stack.can_generate());
+
+    EXPECT_FALSE(stack.can_generate(0));
+    EXPECT_TRUE(stack.can_generate(1));
+    EXPECT_TRUE(stack.can_generate(2));
+    EXPECT_TRUE(stack.can_generate(3));
+    EXPECT_TRUE(stack.can_generate(4));
+
+    stack.m_remote_is_symbol_pivot[1] = true;
+    EXPECT_FALSE(stack.can_generate());
+
+    EXPECT_FALSE(stack.can_generate(0));
+    EXPECT_FALSE(stack.can_generate(1));
+    EXPECT_TRUE(stack.can_generate(2));
+    EXPECT_TRUE(stack.can_generate(3));
+    EXPECT_TRUE(stack.can_generate(4));
+
+    stack.m_remote_is_symbol_pivot[2] = true;
+    EXPECT_FALSE(stack.can_generate());
+
+    EXPECT_FALSE(stack.can_generate(0));
+    EXPECT_FALSE(stack.can_generate(1));
+    EXPECT_FALSE(stack.can_generate(2));
+    EXPECT_TRUE(stack.can_generate(3));
+    EXPECT_TRUE(stack.can_generate(4));
+
+    stack.m_remote_is_symbol_pivot[3] = true;
+    EXPECT_FALSE(stack.can_generate());
+
+    EXPECT_FALSE(stack.can_generate(0));
+    EXPECT_FALSE(stack.can_generate(1));
+    EXPECT_FALSE(stack.can_generate(2));
+    EXPECT_FALSE(stack.can_generate(3));
+    EXPECT_TRUE(stack.can_generate(4));
+
+    // In this case we cannot generate anything useful since the
+    // receiver already has all the information we want to send.
+    // This situation should be avoided in real applications.
+
+    stack.m_remote_is_symbol_pivot[4] = true;
+    EXPECT_FALSE(stack.can_generate());
+
+    EXPECT_FALSE(stack.can_generate(0));
+    EXPECT_FALSE(stack.can_generate(1));
+    EXPECT_FALSE(stack.can_generate(2));
+    EXPECT_FALSE(stack.can_generate(3));
+    EXPECT_FALSE(stack.can_generate(4));
 
 }
