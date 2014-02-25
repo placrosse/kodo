efcf541e14670a0358a3898e3555d5e3e2b86318
Janus Heide <janusheide@gmail.com>
2014-02-25 15:10:33 +0100

    added perpetual generator

---
+++ src/kodo/perpetual/perpetual_generator.hpp
@@ -0,0 +1,148 @@
+// Copyright Steinwurf ApS 2012.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+#pragma once
+
+#include <cstdint>
+
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/uniform_int_distribution.hpp>
+
+#include <fifi/fifi_utils.hpp>
+
+namespace kodo
+{
+    /// @ingroup coefficient_generator_layers
+    /// @brief Generate perpetual coefficients with a specific
+    /// width
+    template<class SuperCoder>
+    class perpetual_generator : public SuperCoder
+    {
+    public:
+
+        /// @copydoc layer::field_type
+        typedef typename SuperCoder::field_type field_type;
+
+        /// @copydoc layer::value_type
+        typedef typename SuperCoder::value_type value_type;
+
+        /// The random generator used
+        typedef boost::random::mt19937 generator_type;
+
+        /// @copydoc layer::seed_type
+        typedef generator_type::result_type seed_type;
+
+    public:
+
+        /// Constructor
+        perpetual_generator()
+            : m_symbol_distribution(field_type::min_value, field_type::max_value),
+              m_pivot_distribution(0, SuperCoder::symbols()),
+              m_width(SuperCoder::symbols()/2)
+            { }
+
+        /// @copydoc layer::generate(uint8_t*)
+        void generate(uint8_t *coefficients)
+        {
+            assert(coefficients != 0);
+
+            // Ensure all coefficients are initially zero
+            std::fill_n( coefficients, SuperCoder::coefficient_vector_size(), 0);
+
+            uint32_t pivot = m_pivot_distribution(m_random_generator);
+            value_type* c = reinterpret_cast<value_type*>(coefficients);
+            fifi::set_value<field_type>(c, pivot, 1);
+
+            uint32_t symbols = SuperCoder::symbols();
+            for(uint32_t i = 1; i <= m_width; ++i)
+            {
+                uint32_t index = (pivot + i) % symbols;
+                coefficients[index] = m_symbol_distribution(m_random_generator);
+            }
+        }
+
+        /// @copydoc layer::generate_partial(uint8_t*)
+        void generate_partial(uint8_t *coefficients)
+        {
+            assert(coefficients != 0);
+
+            // Ensure all coefficients are initially zero
+            std::fill_n( coefficients, SuperCoder::coefficient_vector_size(), 0);
+
+            uint32_t symbols = SuperCoder::symbols();
+
+            //attempt to draw a valid pivot
+            uint32_t pivot = m_pivot_distribution(m_random_generator);
+            uint32_t trials = 1;
+            while((SuperCoder::is_symbol_pivot(pivot)) and (trials <= symbols))
+            {
+                pivot = m_pivot_distribution(m_random_generator);
+                trials++;
+            }
+
+            value_type* c = reinterpret_cast<value_type*>(coefficients);
+            if(SuperCoder::is_symbol_pivot(pivot))
+            {
+                fifi::set_value<field_type>(c, pivot, 1);
+            }
+            else
+            {
+                return;
+            }
+
+            for(uint32_t i = 1; i <= m_width; ++i)
+            {
+                uint32_t index = (pivot + i) % symbols;
+                if(!SuperCoder::is_symbol_pivot(index))
+                {
+                    continue;
+                }
+
+                value_type coefficient =
+                    m_symbol_distribution(m_random_generator);
+
+                fifi::set_value<field_type>(c, index, coefficient);
+            }
+        }
+
+        /// @copydoc layer::seed(seed_type)
+        void seed(seed_type seed_value)
+        {
+            m_random_generator.seed(seed_value);
+        }
+
+        /// Set the width
+        /// @param width the number of non-zero coefficients after the pivot
+        void set_width(uint32_t width)
+            {
+                assert(width <= SuperCoder::symbols());
+                m_width = width;
+            }
+
+        /// Get the width
+        /// @param return the width used by the generator
+        uint32_t width() const
+            {
+                return m_width;
+            }
+
+    private:
+
+        /// The distribution wrapping the random generator
+        boost::random::uniform_int_distribution<value_type>
+        m_symbol_distribution;
+
+        /// Generates uniform indices from the generation
+        boost::random::uniform_int_distribution<uint32_t>
+        m_pivot_distribution;
+
+        /// The random generator
+        boost::random::mt19937 m_random_generator;
+
+        /// The number of non-zero values following the pivot
+        uint32_t m_width;
+
+    };
+}
---
+++ test/src/test_perpetual_generator.cpp
@@ -0,0 +1,64 @@
+// Copyright Steinwurf ApS 2011-2013.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+/// @file test_sparse_uniform_generator.hpp Unit tests for the sparse uniform
+///       coefficient generators
+
+#include <fifi/is_binary.hpp>
+#include "kodo_unit_test/coefficient_generator_helper.hpp"
+#include "../../src/kodo/perpetual/perpetual_generator.hpp"
+
+namespace kodo
+{
+    // Put dummy layers and tests classes in an anonymous namespace
+    // to avoid violations of ODF (one-definition-rule) in other
+    // translation units
+    namespace
+    {
+
+        // Perpetual generator
+        template<class Field>
+        class perpetual_generator_stack :
+            public perpetual_generator<
+                   fake_codec_layer<
+                   coefficient_info<
+                   fake_symbol_storage<
+                   storage_block_info<
+                   finite_field_info<Field,
+                   final_coder_factory<
+                   perpetual_generator_stack<Field>
+                   > > > > > > >
+        { };
+
+        template<class Field>
+        class perpetual_generator_stack_pool :
+            public perpetual_generator<
+                   fake_codec_layer<
+                   coefficient_info<
+                   fake_symbol_storage<
+                   storage_block_info<
+                   finite_field_info<Field,
+                   final_coder_factory_pool<
+                   perpetual_generator_stack_pool<Field>
+                   > > > > > > >
+        { };
+    }
+}
+
+/// Run the tests typical coefficients stack
+TEST(TestCoefficientGenerator, test_perpetual_generator_stack)
+{
+    uint32_t symbols = rand_symbols();
+    uint32_t symbol_size = rand_symbol_size();
+
+    // API tests:
+    run_test<
+        kodo::perpetual_generator_stack,
+        api_generate>(symbols, symbol_size);
+
+    run_test<
+        kodo::perpetual_generator_stack_pool,
+        api_generate>(symbols, symbol_size);
+}
