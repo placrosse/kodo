69247977ad8f2125fa77357c2f4e841a08701666
Morten V. Pedersen <morten@mortenvp.com>
2014-08-06 21:57:17 +0200

    partial mutable shallow storage supported

---
+++ src/kodo/partial_const_shallow_storage_layers.hpp
@@ -0,0 +1,28 @@
+// Copyright Steinwurf ApS 2011-2014.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+#pragma once
+
+#include "partial_shallow_symbol_storage.hpp"
+#include "const_shallow_symbol_storage.hpp"
+#include "storage_bytes_used.hpp"
+#include "storage_block_info.hpp"
+#include "trace_symbol_storage.hpp"
+
+namespace kodo
+{
+    /// @todo Is there a unit test
+    /// @ingroup symbol_storage_layers
+    ///
+    /// @brief Template alias for the common set of mutable shallow storage
+    ///        layers used in most stacks which utilize mutable shallow storage
+    template<class TraceTag, class SuperCoder>
+    using partial_const_shallow_storage_layers =
+        trace_symbol_storage<TraceTag,
+        partial_shallow_symbol_storage<
+        const_shallow_symbol_storage<
+        storage_bytes_used<
+        storage_block_info<SuperCoder> > > > >;
+}
--- src/kodo/partial_mutable_shallow_storage_layers.hpp
+++ src/kodo/partial_mutable_shallow_storage_layers.hpp
@@ -6,22 +6,26 @@
 #pragma once
 
 #include "partial_shallow_symbol_storage.hpp"
-#include "const_shallow_symbol_storage.hpp"
+#include "mutable_shallow_symbol_storage.hpp"
 #include "storage_bytes_used.hpp"
 #include "storage_block_info.hpp"
 #include "trace_symbol_storage.hpp"
 
 namespace kodo
 {
+    /// @todo Is there a unit test. Update we should not make unit
+    /// tests for the using declarations - put this in the
+    /// documentation
+    ///
     /// @ingroup symbol_storage_layers
     ///
     /// @brief Template alias for the common set of mutable shallow storage
     ///        layers used in most stacks which utilize mutable shallow storage
     template<class TraceTag, class SuperCoder>
-    using partial_shallow_storage_layers =
+    using partial_mutable_shallow_storage_layers =
         trace_symbol_storage<TraceTag,
         partial_shallow_symbol_storage<
-        const_shallow_symbol_storage<
+        mutable_shallow_symbol_storage<
         storage_bytes_used<
         storage_block_info<SuperCoder> > > > >;
 }
