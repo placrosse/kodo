216b058782943fd5b93c41cf663ff69ef0d5b53b
Morten V. Pedersen <morten@mortenvp.com>
2014-08-29 15:50:33 +0200

    Adding the source code

---
+++ src/kodo/has_initialize.hpp
@@ -0,0 +1,48 @@
+// Copyright Steinwurf ApS 2011-2013.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+#pragma once
+
+namespace kodo
+{
+    /// @ingroup type_traits
+    ///
+    /// Type trait helper allows compile time detection of whether a
+    /// codec contains a layer with the member function initialize
+    ///
+    /// Example:
+    ///
+    /// typedef kodo::full_rlnc8_encoder encoder_t;
+    ///
+    /// if(kodo::has_initialize<encoder_t>::value)
+    /// {
+    ///     // Do something here
+    /// }
+    ///
+    template<typename T>
+    struct has_initialize
+    {
+    private:
+        typedef std::true_type yes;
+        typedef std::false_type no;
+
+        // The initialize member function has the following prototype:
+        // template<class Factory>
+        // void initialize(Factory& the_factory)
+        //
+        // Because initialize takes a reference we cast 0 to a pointer
+        // and dereference it to get a refence. The fact that we use
+        // int as the type does not matter for the check.
+        template<typename U>
+        static auto test(int) ->
+            decltype(std::declval<U>().initialize(*(int*)0), yes());
+
+        template<typename> static no test(...);
+
+    public:
+
+        enum { value = std::is_same<decltype(test<T>(0)),yes>::value };
+    };
+}
---
+++ test/src/test_has_initialize.cpp
@@ -0,0 +1,65 @@
+// Copyright Steinwurf ApS 2011-2014.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+/// @file test_has_initialize.cpp Unit tests for the
+///       has_initialize class
+
+#include <cstdint>
+
+#include <gtest/gtest.h>
+
+#include <kodo/has_initialize.hpp>
+#include <kodo/rlnc/sliding_window_encoder.hpp>
+
+namespace kodo
+{
+
+    // Put dummy layers and tests classes in an anonymous namespace
+    // to avoid violations of ODF (one-definition-rule) in other
+    // translation units
+    namespace
+    {
+
+        struct dummy_one
+        {
+            template<class Factory>
+            void initialize(Factory& the_factory);
+        };
+
+        struct dummy_two
+        {
+            template<class Factory>
+            void initialize(Factory& the_factory)
+            {
+                the_factory.ok();
+            }
+
+        };
+
+
+        struct dummy_one_parent : public dummy_one
+        { };
+
+        struct dummy_two_parent : public dummy_two
+        { };
+
+        struct dummy_false
+        { };
+
+    }
+}
+
+TEST(TestHasInitialize, detect)
+{
+    EXPECT_FALSE(kodo::has_initialize<uint32_t>::value);
+    EXPECT_FALSE(kodo::has_initialize<kodo::dummy_false>::value);
+    EXPECT_TRUE(kodo::has_initialize<kodo::dummy_one>::value);
+    EXPECT_TRUE(kodo::has_initialize<kodo::dummy_one_parent>::value);
+    EXPECT_TRUE(kodo::has_initialize<kodo::dummy_two>::value);
+    EXPECT_TRUE(kodo::has_initialize<kodo::dummy_two_parent>::value);
+
+    typedef kodo::sliding_window_encoder<fifi::binary8> encoder_type;
+    EXPECT_TRUE(kodo::has_initialize<encoder_type>::value);
+}
