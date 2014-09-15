f22e3ca8252ee9081d6f02b5fa3b6a6eba71aa57
Morten V. Pedersen <morten@mortenvp.com>
2014-09-15 21:58:51 +0200

    Adding unit tests

---
+++ test/src/object/test_mapped_file_sink.cpp
@@ -0,0 +1,90 @@
+// Copyright Steinwurf ApS 2011-2014.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+/// @file test_mapped_file_sink.cpp Unit tests for the
+///       mapped_file_sink class
+
+#include <cstdint>
+#include <memory>
+
+#include <gtest/gtest.h>
+#include <stub/call.hpp>
+
+#include <kodo/object/mapped_file_sink.hpp>
+
+namespace kodo
+{
+// Put dummy layers and tests classes in an anonymous namespace
+// to avoid violations of ODF (one-definition-rule) in other
+// translation units
+namespace
+{
+    /// Helper layer
+    class dummy_layer
+    {
+    public:
+
+        struct factory_base
+        {
+            factory_base(uint32_t symbols, uint32_t symbol_size)
+            {
+                (void) symbols;
+                (void) symbol_size;
+            }
+
+            std::string filename() const
+            {
+                return m_filename();
+            }
+
+            void set_storage(const sak::mutable_storage& storage)
+            {
+                m_set_storage(storage);
+            }
+
+            stub::call<std::string()> m_filename;
+            stub::call<void(const sak::mutable_storage&)> m_set_storage;
+        };
+
+    public:
+
+        template<class Factory>
+        void initialize(Factory& the_factory)
+        {
+            (void) the_factory;
+        }
+
+    };
+
+    // Helper stack
+    class dummy_stack : public object::mapped_file_sink<dummy_layer>
+    { };
+}
+}
+
+/// @todo figure out is this is the right naming
+TEST(ObjectTestMappedFileSink, api)
+{
+    uint32_t symbols = 10;
+    uint32_t symbol_size = 100;
+
+    kodo::dummy_stack stack;
+    kodo::dummy_stack::factory_base factory(symbols, symbol_size);
+
+    factory.m_filename.set_return("mapped_file_sink_one");
+    factory.set_file_size(1234);
+
+    EXPECT_EQ(factory.file_size(), 1234);
+    stack.initialize(factory);
+
+    // @todo check the set_storage function
+
+    // Initialize again
+    factory.m_filename.set_return("mapped_file_sink_two");
+    factory.set_file_size(1234);
+
+    EXPECT_EQ(factory.file_size(), 1234);
+    stack.initialize(factory);
+}
