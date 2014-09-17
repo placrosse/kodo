7661e9e6f78ddde1765114c0c9b34f22a0f5e9c5
Morten V. Pedersen <morten@mortenvp.com>
2014-09-17 16:50:51 +0200

    Working on unit test for file encoder/decoder

--- test/src/object/test_file_encoder.cpp
+++ test/src/object/test_file_encoder.cpp
@@ -23,64 +23,146 @@ namespace kodo
 namespace
 {
     // The encoder we will use
-
-}
-}
-
-template<class SuperTest>
-class setup_file : public SuperTest
-{
-public:
-
-    void run()
+    template<class SuperTest>
+    class setup_file : public SuperTest
     {
-        uint32_t file_size = 23456;
+    public:
 
-        std::string encode_filename = "encode-file.bin";
-        std::string decode_filename = "decode-file.bin";
+        setup_file(uint32_t max_symbols, uint32_t max_symbol_size)
+            : SuperTest(max_symbols, max_symbol_size)
+        { }
 
-        SuperTest::encoder_factory().set_filename(encode_filename);
-        SuperTest::decoder_factory().set_filename(decode_filename);
+        void run()
+        {
+            uint32_t file_size = 23456;
 
-        // Create a test file for encoding.
-        std::ofstream encode_file;
-        encode_file.open (encode_filename, std::ios::binary);
-        std::vector<char> data_in(file_size, 'x');
+            std::string encode_filename = "encode-file.bin";
+            std::string decode_filename = "decode-file.bin";
 
-        encode_file.write(data_in.data(), data_in.size());
-        encode_file.close();
+            SuperTest::encoder_factory().set_filename(encode_filename);
+            SuperTest::decoder_factory().set_filename(decode_filename);
+            SuperTest::decoder_factory().set_file_size(file_size);
 
-        SuperTest::run();
+            // Create a test file for encoding.
+            std::ofstream encode_file;
+            encode_file.open(encode_filename, std::ios::binary);
+            std::vector<char> data_in(file_size, 'x');
 
-        // Read back the decoded data
-        std::ifstream decode_file;
-        decode_file.open (decode_filename, std::ios::binary);
-        std::vector<char> data_out(file_size);
+            encode_file.write(data_in.data(), data_in.size());
+            encode_file.close();
 
-        decode_file.read(data_out.data(), data_out.size());
-        decode_file.close();
+            SuperTest::run();
 
-        EXPECT_TRUE(data_in == data_out);
-   }
-};
+            // Read back the decoded data
+            std::ifstream decode_file;
+            decode_file.open(decode_filename, std::ios::binary);
+            std::vector<char> data_out(file_size);
 
+            decode_file.read(data_out.data(), data_out.size());
+            decode_file.close();
 
-template<class ObjectEncoder, class ObjectDecoder, class SuperTest>
-class object_stacks : public SuperTest
-{
-public:
+            EXPECT_TRUE(data_in == data_out);
+        }
+    };
 
-    // using file_encoder_factory = kodo::object::file_encoder<
-    //     kodo::shallow_full_rlnc_encoder<fifi::binary>>::factory;
+    template<class ObjectEncoder, class ObjectDecoder, class SuperTest>
+    class object_stacks : public SuperTest
+    {
+    public:
 
-    // using file_decoder_factory = kodo::object::file_decoder<
-    //     kodo::shallow_full_rlnc_decoder<fifi::binary>>::factory;
+        using object_encoder_type = ObjectEncoder;
+        using object_decoder_type = ObjectDecoder;
 
-    // file_encoder_factory encoder_factory(max_symbols, max_symbol_size);
-    // file_decoder_factory decoder_factory(max_symbols, max_symbol_size);
+        using object_encoder_factory_type =
+            typename object_encoder_type::factory;
 
+        using object_decoder_factory_type =
+            typename object_decoder_type::factory;
 
-};
+    public:
+
+        object_stacks(uint32_t max_symbols, uint32_t max_symbol_size)
+            : SuperTest(max_symbols, max_symbol_size),
+              m_encoder_factory(max_symbols, max_symbol_size),
+              m_decoder_factory(max_symbols, max_symbol_size)
+        { }
+
+        object_encoder_factory_type& encoder_factory()
+        {
+            return m_encoder_factory;
+        }
+
+        object_decoder_factory_type& decoder_factory()
+        {
+            return m_decoder_factory;
+        }
+
+    private:
+
+        object_encoder_factory_type m_encoder_factory;
+        object_decoder_factory_type m_decoder_factory;
+    };
+
+    template<class SuperTest>
+    class basic_run : public SuperTest
+    {
+    public:
+
+        basic_run(uint32_t max_symbols, uint32_t max_symbol_size)
+            : SuperTest(max_symbols, max_symbol_size)
+        { }
+
+        void run()
+        {
+
+            auto file_encoder = SuperTest::encoder_factory().build();
+            auto file_decoder = SuperTest::decoder_factory().build();
+
+            EXPECT_EQ(file_encoder->blocks(), file_decoder->blocks());
+
+            for (uint32_t i = 0; i < file_encoder->blocks(); ++i)
+            {
+                auto e = file_encoder->build(i);
+                auto d = file_decoder->build(i);
+
+                std::vector<uint8_t> payload(e->payload_size());
+
+                while (!d->is_complete())
+                {
+                    e->encode( payload.data() );
+
+                    // Here we would send and receive the payload over a
+                    // network. Lets throw away some packet to simulate.
+                    if (rand() % 2)
+                    {
+                        continue;
+                    }
+
+                    d->decode( payload.data() );
+                }
+            }
+        }
+    };
+
+    class final
+    {
+    public:
+        final(uint32_t max_symbols, uint32_t max_symbol_size)
+        {
+            (void) max_symbols;
+            (void) max_symbol_size;
+        }
+    };
+
+    template<class ObjectEncoder, class ObjectDecoder>
+    using test_file =
+        setup_file<
+        basic_run<
+        object_stacks<ObjectEncoder, ObjectDecoder,
+        final>>>;
+
+}
+}
 
 TEST(ObjectTestFileEncoder, api)
 {
@@ -91,65 +173,12 @@ TEST(ObjectTestFileEncoder, api)
 
     uint32_t file_size = 23456;
 
-    std::string encode_filename = "encode-file.bin";
-    std::string decode_filename = "decode-file.bin";
+    using encoder = kodo::object::file_encoder<
+        kodo::shallow_full_rlnc_encoder<fifi::binary>>;
 
-    // Create a test file for encoding.
-    std::ofstream encode_file;
-    encode_file.open (encode_filename, std::ios::binary);
-    std::vector<char> data_in(file_size, 'x');
+    using decoder = kodo::object::file_decoder<
+        kodo::shallow_full_rlnc_decoder<fifi::binary>>;
 
-    encode_file.write(data_in.data(), data_in.size());
-    encode_file.close();
-
-    // Actual encoding/decoding of the file
-
-    using file_encoder_factory = kodo::object::file_encoder<
-        kodo::shallow_full_rlnc_encoder<fifi::binary>>::factory;
-
-    using file_decoder_factory = kodo::object::file_decoder<
-        kodo::shallow_full_rlnc_decoder<fifi::binary>>::factory;
-
-    file_encoder_factory encoder_factory(max_symbols, max_symbol_size);
-    file_decoder_factory decoder_factory(max_symbols, max_symbol_size);
-
-    encoder_factory.set_filename(encode_filename);
-
-    decoder_factory.set_filename(decode_filename);
-    decoder_factory.set_file_size(file_size);
-
-    auto file_encoder = encoder_factory.build();
-    auto file_decoder = decoder_factory.build();
-
-    for (uint32_t i = 0; i < file_encoder->blocks(); ++i)
-    {
-        auto e = file_encoder->build(i);
-        auto d = file_decoder->build(i);
-
-        std::vector<uint8_t> payload(e->payload_size());
-
-        while (!d->is_complete())
-        {
-            e->encode( payload.data() );
-
-            // Here we would send and receive the payload over a
-            // network. Lets throw away some packet to simulate.
-            if (rand() % 2)
-            {
-                continue;
-            }
-
-            d->decode( payload.data() );
-        }
-    }
-
-    // Read back the decoded data
-    std::ifstream decode_file;
-    decode_file.open (decode_filename, std::ios::binary);
-    std::vector<char> data_out(file_size);
-
-    decode_file.read(data_out.data(), data_out.size());
-    decode_file.close();
-
-    EXPECT_TRUE(data_in == data_out);
+    kodo::test_file<encoder, decoder> test(max_symbols, max_symbol_size);
+    test.run();
 }
