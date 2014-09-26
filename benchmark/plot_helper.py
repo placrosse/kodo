e5f4bae1260bfe92839ac5355b3f86d728dbbba9
Morten V. Pedersen <morten@mortenvp.com>
2014-09-26 10:03:55 +0200

    Removing last year in copyright

--- benchmark/plot_helper.py
+++ benchmark/plot_helper.py
@@ -1,5 +1,5 @@
 """
-Copyright Steinwurf ApS 2011-2013.
+Copyright Steinwurf ApS 2011.
 Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
 See accompanying file LICENSE.rst or
 http://www.steinwurf.com/licensing
