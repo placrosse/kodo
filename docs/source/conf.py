21a1be7ea5c0cf404f0f09ef4d89bbf87f40819f
Jeppe Pihl <jpihl08@gmail.com>
2014-10-01 15:30:38 +0200

    use rtd theme if available

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -176,6 +176,13 @@ html_static_path = []
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'Kododoc'
 
+try:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+except ImportError:
+    print("Unable to use sphinx_rtd_theme falling back to default.")
+    pass
 
 # -- Options for LaTeX output -------------------------------------------------
 
