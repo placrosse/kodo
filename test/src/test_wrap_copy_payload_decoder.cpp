0be228919d1c8b8025e594ac67258cb749201868
nestorjhernandezm <nestor@steinwurf.com>
2014-10-15 12:45:47 +0200

    Adding wrap layer for copy payload decoder

---
+++ test/src/test_wrap_copy_payload_decoder.cpp
@@ -0,0 +1,8 @@
+// Copyright Steinwurf ApS 2014.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+/// @file test_wrap_copy_payload_decoder.cpp The unit tests for the
+///       wrap_copy_payload_decoder layer is defined in the
+///       test_copy_payload_decoder.cpp
